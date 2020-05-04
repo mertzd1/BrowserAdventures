ab4fc57969c582758a2337c0684ae64b7695c429
Rayosama <59894092+Rayosama@users.noreply.github.com>
2020-05-04 08:52:52 -0400

    Just minor Changes

---
+++ BrowserAdventures/EnemiesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BrowserAdventures.Models;
+using browsersqlserver.database.windows;
+
+namespace BrowserAdventures
+{
+    public class EnemiesController : Controller
+    {
+        private readonly net _context;
+
+        public EnemiesController(net context)
+        {
+            _context = context;
+        }
+
+        // GET: Enemies
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Enemy.ToListAsync());
+        }
+
+        // GET: Enemies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var enemy = await _context.Enemy
+                .FirstOrDefaultAsync(m => m.EnemyID == id);
+            if (enemy == null)
+            {
+                return NotFound();
+            }
+
+            return View(enemy);
+        }
+
+        // GET: Enemies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Enemies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EnemyID,EnemyName,EnemyDescription,EnemyHealth,EnemyMultipler,EnemyDie,EnemyModifier,EnemyExperience")] Enemy enemy)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(enemy);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(enemy);
+        }
+
+        // GET: Enemies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var enemy = await _context.Enemy.FindAsync(id);
+            if (enemy == null)
+            {
+                return NotFound();
+            }
+            return View(enemy);
+        }
+
+        // POST: Enemies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EnemyID,EnemyName,EnemyDescription,EnemyHealth,EnemyMultipler,EnemyDie,EnemyModifier,EnemyExperience")] Enemy enemy)
+        {
+            if (id != enemy.EnemyID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(enemy);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EnemyExists(enemy.EnemyID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(enemy);
+        }
+
+        // GET: Enemies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var enemy = await _context.Enemy
+                .FirstOrDefaultAsync(m => m.EnemyID == id);
+            if (enemy == null)
+            {
+                return NotFound();
+            }
+
+            return View(enemy);
+        }
+
+        // POST: Enemies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var enemy = await _context.Enemy.FindAsync(id);
+            _context.Enemy.Remove(enemy);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EnemyExists(int id)
+        {
+            return _context.Enemy.Any(e => e.EnemyID == id);
+        }
+    }
+}
